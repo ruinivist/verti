5d366edf5ae77e3a88677450d1402a2f345ec389
ruinivist <ruinivist@proton.me>
2026-03-07 16:31:07 +0000

    NUKE: we are rewriting it, the current impl was no good

--- go.mod
+++
@@ -1,7 +0,0 @@
-module verti
-
-go 1.23
-
-require github.com/BurntSushi/toml v1.4.0
-
-require github.com/google/uuid v1.6.0
--- internal/snapshots/collection.go
+++
@@ -1,9 +0,0 @@
-package snapshots
-
-const internalCollectionTmpDir = ".tmp"
-
-// IsInternalCollectionDir reports whether a collection directory name is
-// reserved for internal snapshot/orphan staging.
-func IsInternalCollectionDir(name string) bool {
-	return name == internalCollectionTmpDir
-}
